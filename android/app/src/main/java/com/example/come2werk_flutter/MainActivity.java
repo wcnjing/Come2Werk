4a802fab562ccf408deb4386a40e0856b0c1b796
Xi Shawn Timothy Tey <s10243964@connect.np.edu.sg>
2024-01-28 21:31:20 +0800

    Added packages to MainActivity

--- android/app/src/main/java/com/example/come2werk_flutter/MainActivity.java
+++ android/app/src/main/java/com/example/come2werk_flutter/MainActivity.java
@@ -1,6 +1,33 @@
 package com.example.come2werk_flutter;
 
 import io.flutter.embedding.android.FlutterActivity;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.UUID;
+
+import static android.content.ContentValues.TAG;
 
 public class MainActivity extends FlutterActivity {
 }
