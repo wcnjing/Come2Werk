05e520868ce8adb40b281cfecb5a7dc514622352
wcnjing <wenjinx2005@gmail.com>
2024-01-30 04:57:01 +0800

    added cards

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -12,48 +12,61 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-          colorSchemeSeed: const Color(0xff6750a4), useMaterial3: true),
-      home: Scaffold(
-        appBar: AppBar(title: const Text('Divider Sample')),
-        body: const DividerExample(),
-      ),
-    );
-  }
-}
+    final welcomeback = Text("Welcome Back, Jeremy Chan!",
+      textAlign: TextAlign.center,
+      style: TextStyle(
+        fontSize: 30,
+        fontWeight: FontWeight.bold,
+      ),);
 
-class DividerExample extends StatelessWidget {
-  const DividerExample({super.key});
+
+
+
+    return Scaffold(body: Center(
+      child:SingleChildScrollView(
+        child: Container(
+          child: Padding(
+            padding: const EdgeInsets.all(36.0),
+            child: Column(
+              children: <Widget>[
+
+                SizedBox(
+                    height:200,
+                    child: Image.asset("assets/profilepic.png",
+                      fit:BoxFit.contain,)
+                ),
+                SizedBox(height:50),
+                welcomeback,
+                ElevatedCardExample(),
+                ElevatedCardExample()
+
+
+              ],
+
+            ),
+          ),
+        ),
+      ),
+    ),
+    );
+}
+}
+class ElevatedCardExample extends StatelessWidget {
+  const ElevatedCardExample({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const Center(
-      child: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          children: <Widget>[
-
-            Expanded(
-              child: Card(
-
-              ),
-            ),
-            Divider(),
-            Expanded(
-              child: Card(
-                child: SizedBox.expand(),
-              ),
-            ),
-          ],
+      child: Card(
+        child: SizedBox(
+          width: 300,
+          height: 100,
+          child: Center(child: Text('Elevated Card')),
         ),
       ),
     );
   }
-
-
-
-  }
+}
 
 
 
