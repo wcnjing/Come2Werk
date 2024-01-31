ddfe44733280f14c1e7a1aab9a7a06ea37317fe3
wcnjing <wenjinx2005@gmail.com>
2024-01-31 20:12:03 +0800

    added calendar for attendance page

--- lib/screens/MyAttendancePage.dart
+++ lib/screens/MyAttendancePage.dart
@@ -1,15 +1,63 @@
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 
-class AttendanceScreen extends StatelessWidget {
+class AttendanceScreen extends StatefulWidget {
+  @override
+  _AttendanceScreenState createState() => _AttendanceScreenState();
+}
+
+class _AttendanceScreenState extends State<AttendanceScreen> {
+  late DateTime _selectedDate;
+  late DateTime _focusedDay;
+  late DateTime _firstDay;
+  late DateTime _lastDay;
+
+  @override
+  void initState() {
+    super.initState();
+    _selectedDate = DateTime.now();
+    _focusedDay = DateTime.now();
+    _firstDay = DateTime.now().subtract(Duration(days: 30)); // Adjust as needed
+    _lastDay = DateTime.now().add(Duration(days: 30)); // Adjust as needed
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Attendance'),
       ),
-      body: Center(
-        child: Text('This is the destination page!'),
+      body: Column(
+        children: [
+          TableCalendar(
+            firstDay: _firstDay,
+            lastDay: _lastDay,
+            focusedDay: _focusedDay,
+            selectedDayPredicate: (day) {
+              // Return true if the day is selected
+              return isSameDay(_selectedDate, day);
+            },
+            onDaySelected: (selectedDay, focusedDay) {
+              setState(() {
+                _selectedDate = selectedDay;
+                _focusedDay = focusedDay; // Update focused day as well
+              });
+            },
+          ),
+          SizedBox(height: 20),
+          Center(
+            child: Text(
+              'Selected Date: ${_selectedDate.toLocal()}',
+              style: TextStyle(fontSize: 18),
+            ),
+          ),
+        ],
       ),
     );
   }
+
+  bool isSameDay(DateTime a, DateTime b) {
+    return a.year == b.year && a.month == b.month && a.day == b.day;
+  }
 }
+
