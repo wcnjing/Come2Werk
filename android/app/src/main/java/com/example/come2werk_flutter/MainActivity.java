ba0f0bae3b2214e444afc7778c109c304faaaae1
Ng Wen Jing <115871755+wcnjing@users.noreply.github.com>
2024-02-01 16:54:22 +0800

    added firebase gradle

--- android/app/src/main/java/com/example/come2werk_flutter/MainActivity.java
+++ android/app/src/main/java/com/example/come2werk_flutter/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.come2werk_flutter;
 
 import io.flutter.embedding.android.FlutterActivity;
 
+import android.app.Application;
 public class MainActivity extends FlutterActivity {
 }
 
