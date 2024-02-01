b80548f9a0e8cf4378f3036e31412bf1ff038508
Ng Wen Jing <115871755+wcnjing@users.noreply.github.com>
2024-02-01 16:55:10 +0800

    Revert "added firebase gradle"
    This reverts commit ba0f0bae3b2214e444afc7778c109c304faaaae1.

--- android/app/src/main/java/com/example/come2werk_flutter/MainActivity.java
+++ android/app/src/main/java/com/example/come2werk_flutter/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.come2werk_flutter;
 
 import io.flutter.embedding.android.FlutterActivity;
 
-import android.app.Application;
 public class MainActivity extends FlutterActivity {
 }
 
