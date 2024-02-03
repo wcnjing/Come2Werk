e07852720c10ae86dbfcad646bdbc98e73f86b0c
Ng Wen Jing <115871755+wcnjing@users.noreply.github.com>
2024-02-03 09:34:59 +0800

    added gantry function

--- lib/screens/MyAttendancePage.dart
+++ lib/screens/MyAttendancePage.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_database/firebase_database.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -88,7 +90,7 @@ class _AttendanceScreenState extends State<AttendanceScreen> {
             focusedDay: _focusedDay,
             selectedDayPredicate: (day) {
               // Return true if the day is selected or highlighted
-              return isSameDay(_selectedDate, day) || _highlightedDates.contains(day);
+              return isSameDay(_selectedDate, day) || _highlightedDates.contains(day.toLocal());
             },
             onDaySelected: (selectedDay, focusedDay) {
               setState(() {
@@ -98,7 +100,11 @@ class _AttendanceScreenState extends State<AttendanceScreen> {
             },
             calendarStyle: CalendarStyle(
               selectedDecoration: BoxDecoration(
-                color: Colors.red, // Highlight color
+                color: Colors.blueAccent, // Selected color
+                shape: BoxShape.circle,
+              ),
+              todayDecoration: BoxDecoration(
+                color: Colors.blue, // Today's color
                 shape: BoxShape.circle,
               ),
             ),
@@ -117,22 +123,50 @@ class _AttendanceScreenState extends State<AttendanceScreen> {
               if (isSameDay(_selectedDate, DateTime.now())) {
                 DatabaseReference _attendanceRef = FirebaseDatabase.instance.ref().child('Attendance');
                 _attendanceRef.child(_selectedDate.toString().split(' ')[0]).once().then((snapshot) {
-                  if (snapshot != null && snapshot.snapshot.value != null && (snapshot.snapshot.value as Map)['checkIn'] == true) {
-
+                  if (snapshot != null &&
+                      snapshot.snapshot.value != null &&
+                      (snapshot.snapshot.value as Map)['checkIn'] == true) {
                     // If the date is already checked in, remove it from both local set and Firebase
-                    _highlightedDates.remove(_selectedDate);
+                    _highlightedDates.remove(_selectedDate.toLocal());
                     _attendanceRef.child(_selectedDate.toString().split(' ')[0]).remove();
                   } else {
-                    // Toggle highlighting of selected date
-                    _highlightedDates.add(_selectedDate);
-                    // Add or update the check-in status in Firebase
-                    _attendanceRef.child(_selectedDate.toString().split(' ')[0]).set({'checkIn': true});
+                    // Show the dialog for Gantry selection
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return AlertDialog(
+                          title: Text('Select Gantry'),
+                          content: Column(
+                            children: [
+                              ElevatedButton(
+                                onPressed: () {
+                                  handleGantrySelection('A');
+                                },
+                                child: Text('Gantry A'),
+                              ),
+                              ElevatedButton(
+                                onPressed: () {
+                                  handleGantrySelection('B');
+                                },
+                                child: Text('Gantry B'),
+                              ),
+                              ElevatedButton(
+                                onPressed: () {
+                                  handleGantrySelection('C');
+                                },
+                                child: Text('Gantry C'),
+                              ),
+                            ],
+                          ),
+                        );
+                      },
+                    );
                   }
                 }).catchError((error) {
                   print('Failed to check attendance: $error');
                 });
               } else {
-                // Display a message or take appropriate action for invalid date
+                // Display a message or take appropriate action for an invalid date
                 showDialog(
                   context: context,
                   builder: (BuildContext context) {
@@ -154,6 +188,7 @@ class _AttendanceScreenState extends State<AttendanceScreen> {
             },
             child: Text('Check In'),
           ),
+
           SizedBox(height: 20),
           Card(
             elevation: 4,
@@ -182,18 +217,52 @@ class _AttendanceScreenState extends State<AttendanceScreen> {
       ),
     );
   }
+  void handleGantrySelection(String gantry) {
+    // Perform actions based on the selected gantry
+    print('Selected Gantry: $gantry');
+
+    // Add or update the check-in status in Firebase with the selected gantry
+    DatabaseReference _attendanceRef = FirebaseDatabase.instance.ref().child('Attendance');
+    _attendanceRef.child(_selectedDate.toString().split(' ')[0]).set({
+      'checkIn': true,
+      'gantry': gantry,
+    });
+
+    // Toggle highlighting of selected date
+    _highlightedDates.add(_selectedDate.toLocal());
+
+    Navigator.pop(context); // Close the dialog
+
+    // Show "Please scan your card now" message with a delay
+    Fluttertoast.showToast(
+      msg: 'Please scan your card now',
+      toastLength: Toast.LENGTH_SHORT,
+      gravity: ToastGravity.CENTER,
+    );
+
+    // Introduce a delay before showing the "Check-in successful" toast
+    Future.delayed(Duration(seconds: 5), () {
+      // Show "Check-in successful" toast
+      Fluttertoast.showToast(
+        msg: 'Check-in successful',
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.CENTER,
+      );
+    });
+
+    setState(() {}); // Update the UI
+  }
+
 
   bool isSameDay(DateTime a, DateTime b) {
     return a.year == b.year && a.month == b.month && a.day == b.day;
   }
 
   int countHighlightedDays() {
-    DateTime startDate = DateTime.now().subtract(Duration(days: 30));
-    DateTime endDate = DateTime.now().add(Duration(days: 30));
-
+    DateTime startOfMonth = DateTime(DateTime.now().year, DateTime.now().month, 1);
     int count = 0;
     for (DateTime date in _highlightedDates) {
-      if (date.isAfter(startDate) && date.isBefore(endDate)) {
+      if ((date.isAfter(startOfMonth) || date == (startOfMonth)) && date.isBefore(DateTime.now())) {
         count++;
       }
     }
@@ -202,13 +271,8 @@ class _AttendanceScreenState extends State<AttendanceScreen> {
 
   int countNonHighlightedDays() {
     DateTime firstDayOfMonth = DateTime(DateTime.now().year, DateTime.now().month, 1);
-    int count = -1;
+    int totalDays = firstDayOfMonth.difference(DateTime.now()).inDays.abs() +1;
 
-    for (DateTime date = firstDayOfMonth; date.isBefore(DateTime.now()); date = date.add(Duration(days: 1))) {
-      if (!_highlightedDates.contains(date)) {
-        count++;
-      }
-    }
-    return count;
+    return totalDays - countHighlightedDays();
   }
 }
